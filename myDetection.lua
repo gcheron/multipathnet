b57a7f1afcaf9d28c4cfcdd1b25a26c1054f7310
gcheron <gcheron@sequoia.cm.cluster>
2016-09-26 17:02:53 +0200

    add option not to compute detections corresponding to already existing resulting files

--- myDetection.lua
+++ myDetection.lua
@@ -29,6 +29,7 @@ cmd:option('-imglist','' ,'path/to/test/image/list.txt')
 cmd:option('-imgrespath','./res.jpg' ,'path/to/result/image')
 cmd:option('-boxrespath','./resbox.jpg' ,'path/to/result/box')
 cmd:option('-resinfolders',false,'create foldername (found before imagename input) to save results')
+cmd:option('-checkexisting',false,'do not compute if an existing box result is found')
 cmd:option('-gtboxpath','' ,'path/to/GT/box')
 cmd:option('-thr', 0.5, 'multipathnet score threshold [0,1]')
 cmd:option('-maxsize', 600, 'resize image dimension')
@@ -111,136 +112,142 @@ end
 local prev_time = 0 ;
 local time = torch.tic() ;
 for im_i = config.start_id,#imagelist do
-------------------- Run DeepMask --------------------
-
-   local img = image.load(imagelist[im_i])
-   local imgOri = img:clone() ;
-
-   local original_scale = config.maxsize/math.max(img:size(2),img:size(3))
-   --print('original scale:' .. original_scale)
-   --print('from size: ' .. img:size(2),img:size(3))
-   img = image.scale(img, config.maxsize)
-   local h,w = img:size(2),img:size(3)
-   --print('to size: ' .. img:size(2),img:size(3))
-
-   infer:forward(img)
-
-   local masks,_ = infer:getTopProps(.2,h,w)
-
-   local Rs = coco.MaskApi.encode(masks)
-   local bboxes = coco.MaskApi.toBbox(Rs)
-   bboxes:narrow(2,3,2):add(bboxes:narrow(2,1,2)) -- convert from x,y,w,h to x1,y1,x2,y2
-   --print(bboxes)
-
-------------------- Run MultiPathNet --------------------
-
-   local detections = detector:detect(img:float(), bboxes:float())
-   local prob, maxes = detections:max(2)
-   --print(maxes)
-
-   -- remove background detections
-   local nonbg_idx = maxes:squeeze():gt(1):cmul(prob:gt(config.thr)):nonzero()
-   local idx,scored_boxes,final_idx
-
-   local selected_index = {} ; -- [1] = prob_id ; [2] = area_id
-
-   local resbox = {} ;
-
-   if not(nonbg_idx:size():size()==0) then -- if there is at least one detection
-      idx = nonbg_idx:select(2,1)
-      bboxes = bboxes:index(1, idx)
-      maxes = maxes:index(1, idx)
-      prob = prob:index(1, idx)
-
-      scored_boxes = torch.cat(bboxes:float(), prob:float(), 2)
-      final_idx = utils.nms_dense(scored_boxes, 0.3)
-
-------------------- Select human detections --------------------
-
-      local person_prob = -1 ; -- store best score
-      local person_area = -1 ; -- sotre largest box
-   
-      for i,v in ipairs(final_idx:totable()) do
-         local class = maxes[v][1]-1
-         local name = dataset.dataset.categories[class]
-         if name == "person" then
-            local x1,y1,x2,y2 = table.unpack(bboxes[v]:totable())
-            local wb,hb = x2-x1,y2-y1 
-   
-            local cprob = prob[v][1]
-            local carea = wb*hb ;
-            if cprob > person_prob then
-               person_prob = cprob
-               selected_index[1] = v ;
-            end 
-            if carea > person_area then
-               person_area = carea 
-               selected_index[2] = v ;
-            end
-          end
-      end
-      -- save all detections above th
-      resbox.boxes_scores_classes=torch.cat(bboxes:double()/original_scale, torch.cat(prob:double(),maxes:double():add(-1), 2),2)
-    end
-------------------- Save & Draw detections --------------------
-   h,w=imgOri:size(2),imgOri:size(3)
-
-   -- save ground truth
-   local gtmask,res 
-   if draw then res = imgOri end
-
-   if config.gtboxpath ~= '' then
-      local gtbox = torch.load(gtboxlist[im_i])
-      resbox.GT = gtbox:double() ;
-
-      local x1g,y1g,x2g,y2g = table.unpack(gtbox:totable())
-      local wbg,hbg = x2g-x1g,y2g-y1g
-      gtmask = coco.MaskApi.frBbox(torch.Tensor{x1g,y1g,wbg,hbg},h,w) ;
- 
-      if draw then
-         coco.MaskApi.drawMasks(res, coco.MaskApi.decode(gtmask))
-         y2g = math.min(y2g, res:size(2)) - 10
-         myDrawText(res,x1g,y2g,'GT',imagereslist[im_i])
-      end
+   local compute = true ;
+   if config.checkexisting then
+      local f=io.open(boxreslist[im_i],"r")
+      if f~=nil then io.close(f) ; compute = false ; end
    end
+   if compute then
+   ------------------- Run DeepMask --------------------
 
-   if selected_index[1] then -- if a human detection exists
-      local v=selected_index[1]
-      local cbox = bboxes[v]/original_scale
-      resbox.score = cbox
+      local img = image.load(imagelist[im_i])
+      local imgOri = img:clone() ;
+   
+      local original_scale = config.maxsize/math.max(img:size(2),img:size(3))
+      --print('original scale:' .. original_scale)
+      --print('from size: ' .. img:size(2),img:size(3))
+      img = image.scale(img, config.maxsize)
+      local h,w = img:size(2),img:size(3)
+      --print('to size: ' .. img:size(2),img:size(3))
+   
+      infer:forward(img)
+   
+      local masks,_ = infer:getTopProps(.2,h,w)
+   
+      local Rs = coco.MaskApi.encode(masks)
+      local bboxes = coco.MaskApi.toBbox(Rs)
+      bboxes:narrow(2,3,2):add(bboxes:narrow(2,1,2)) -- convert from x,y,w,h to x1,y1,x2,y2
+      --print(bboxes)
+   
+   ------------------- Run MultiPathNet --------------------
+   
+      local detections = detector:detect(img:float(), bboxes:float())
+      local prob, maxes = detections:max(2)
+      --print(maxes)
+   
+      -- remove background detections
+      local nonbg_idx = maxes:squeeze():gt(1):cmul(prob:gt(config.thr)):nonzero()
+      local idx,scored_boxes,final_idx
+   
+      local selected_index = {} ; -- [1] = prob_id ; [2] = area_id
+   
+      local resbox = {} ;
+   
+      if not(nonbg_idx:size():size()==0) then -- if there is at least one detection
+         idx = nonbg_idx:select(2,1)
+         bboxes = bboxes:index(1, idx)
+         maxes = maxes:index(1, idx)
+         prob = prob:index(1, idx)
+   
+         scored_boxes = torch.cat(bboxes:float(), prob:float(), 2)
+         final_idx = utils.nms_dense(scored_boxes, 0.3)
+   
+   ------------------- Select human detections --------------------
+   
+         local person_prob = -1 ; -- store best score
+         local person_area = -1 ; -- sotre largest box
       
-      local x1,y1,x2,y2 = table.unpack(cbox:totable())
-      local wb,hb = x2-x1,y2-y1
-      local cmask = coco.MaskApi.frBbox(torch.Tensor{x1,y1,wb,hb},h,w) ;
-      if config.gtboxpath ~= '' then resbox.score_iou = coco.MaskApi.iou(cmask,gtmask)[1][1] ; end
-
-      if draw then
-         coco.MaskApi.drawMasks(res, coco.MaskApi.decode(cmask))
-         y2 = math.min(y2, res:size(2)) - 10
-         if config.gtboxpath ~= '' then myDrawText(res,x1,y2,('score %.2f'):format(resbox.score_iou),imagereslist[im_i]) end
+         for i,v in ipairs(final_idx:totable()) do
+            local class = maxes[v][1]-1
+            local name = dataset.dataset.categories[class]
+            if name == "person" then
+               local x1,y1,x2,y2 = table.unpack(bboxes[v]:totable())
+               local wb,hb = x2-x1,y2-y1 
+      
+               local cprob = prob[v][1]
+               local carea = wb*hb ;
+               if cprob > person_prob then
+                  person_prob = cprob
+                  selected_index[1] = v ;
+               end 
+               if carea > person_area then
+                  person_area = carea 
+                  selected_index[2] = v ;
+               end
+             end
+         end
+         -- save all detections above th
+         resbox.boxes_scores_classes=torch.cat(bboxes:double()/original_scale, torch.cat(prob:double(),maxes:double():add(-1), 2),2)
+       end
+   ------------------- Save & Draw detections --------------------
+      h,w=imgOri:size(2),imgOri:size(3)
+   
+      -- save ground truth
+      local gtmask,res 
+      if draw then res = imgOri end
+   
+      if config.gtboxpath ~= '' then
+         local gtbox = torch.load(gtboxlist[im_i])
+         resbox.GT = gtbox:double() ;
+   
+         local x1g,y1g,x2g,y2g = table.unpack(gtbox:totable())
+         local wbg,hbg = x2g-x1g,y2g-y1g
+         gtmask = coco.MaskApi.frBbox(torch.Tensor{x1g,y1g,wbg,hbg},h,w) ;
+    
+         if draw then
+            coco.MaskApi.drawMasks(res, coco.MaskApi.decode(gtmask))
+            y2g = math.min(y2g, res:size(2)) - 10
+            myDrawText(res,x1g,y2g,'GT',imagereslist[im_i])
+         end
       end
-
-      v=selected_index[2]
-      cbox = bboxes[v]/original_scale
-      resbox.area = cbox
-
-      x1,y1,x2,y2 = table.unpack(cbox:totable())
-      wb,hb = x2-x1,y2-y1
-      cmask = coco.MaskApi.frBbox(torch.Tensor{x1,y1,wb,hb},h,w) ;
-      if config.gtboxpath ~= '' then resbox.area_iou = coco.MaskApi.iou(cmask,gtmask)[1][1] ; end
- 
-      if draw then
-         coco.MaskApi.drawMasks(res, coco.MaskApi.decode(cmask))
-         y2 = math.min(y2, res:size(2)) - 10
-         if config.gtboxpath ~= '' then myDrawText(res,x1,y2,('area %.2f'):format(resbox.area_iou),imagereslist[im_i]) end
+   
+      if selected_index[1] then -- if a human detection exists
+         local v=selected_index[1]
+         local cbox = bboxes[v]/original_scale
+         resbox.score = cbox
+         
+         local x1,y1,x2,y2 = table.unpack(cbox:totable())
+         local wb,hb = x2-x1,y2-y1
+         local cmask = coco.MaskApi.frBbox(torch.Tensor{x1,y1,wb,hb},h,w) ;
+         if config.gtboxpath ~= '' then resbox.score_iou = coco.MaskApi.iou(cmask,gtmask)[1][1] ; end
+   
+         if draw then
+            coco.MaskApi.drawMasks(res, coco.MaskApi.decode(cmask))
+            y2 = math.min(y2, res:size(2)) - 10
+            if config.gtboxpath ~= '' then myDrawText(res,x1,y2,('score %.2f'):format(resbox.score_iou),imagereslist[im_i]) end
+         end
+   
+         v=selected_index[2]
+         cbox = bboxes[v]/original_scale
+         resbox.area = cbox
+   
+         x1,y1,x2,y2 = table.unpack(cbox:totable())
+         wb,hb = x2-x1,y2-y1
+         cmask = coco.MaskApi.frBbox(torch.Tensor{x1,y1,wb,hb},h,w) ;
+         if config.gtboxpath ~= '' then resbox.area_iou = coco.MaskApi.iou(cmask,gtmask)[1][1] ; end
+    
+         if draw then
+            coco.MaskApi.drawMasks(res, coco.MaskApi.decode(cmask))
+            y2 = math.min(y2, res:size(2)) - 10
+            if config.gtboxpath ~= '' then myDrawText(res,x1,y2,('area %.2f'):format(resbox.area_iou),imagereslist[im_i]) end
+         end
+      else
+         resbox.score_iou=0
+         resbox.area_iou=0
       end
-   else
-      resbox.score_iou=0
-      resbox.area_iou=0
+      torch.save(boxreslist[im_i],resbox)
+      if draw then image.save(imagereslist[im_i],res) end
    end
-   torch.save(boxreslist[im_i],resbox)
-   if draw then image.save(imagereslist[im_i],res) end
-
    local measured_time = torch.toc(time) ;
    local estimated_time = (measured_time/(im_i-config.start_id+1))*(#imagelist-im_i) ; -- estimate remaining time
    print(('%d out of %d images: %.2f s (remaining time: %.0f s)'):format(im_i,#imagelist,measured_time-prev_time,estimated_time))
